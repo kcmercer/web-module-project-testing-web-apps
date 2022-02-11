7d82065256c152acceacc18517a4f1d3481ceb1b
Kenneth Mercer <kmercer5439@gmail.com>
2022-02-10 20:55:56 -0600

    solved test 3

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -20,28 +20,35 @@ test('renders the contact form header', ()=> {
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+  render(<ContactForm />)
+
+  const firstNameInput = screen.queryByPlaceholderText('Edd')
+  userEvent.type(firstNameInput, 'x');
+
+  const fniError = screen.queryByText(/firstName must have at least 5 characters./i)
+  expect(fniError).toBeInTheDocument();
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+  render(<ContactForm />)
 });
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
+  render(<ContactForm />)
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
+  render(<ContactForm />)
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
+  render(<ContactForm />)
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
+  render(<ContactForm />)
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
+  render(<ContactForm />)
 });
